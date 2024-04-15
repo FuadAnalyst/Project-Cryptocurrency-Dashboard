cd9a80b21ad4ac4225f8c7a372987c7d7cbeeead
Fuad Guseynov <156589453+FuadAnalyst@users.noreply.github.com>
2024-04-15 19:30:28 +0300

    Add files via upload

---
+++ data_script.py
@@ -0,0 +1,27 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import pandas as pd
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+    'start':'1',
+    'limit':'250',
+    'convert':'USD'
+}
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': 'c1eb267d-c062-462f-a06f-00e87cf11c8a',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+
+
+df2 = pd.json_normalize(data['data'])
